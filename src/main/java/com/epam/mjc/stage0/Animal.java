36367844da18895c9e4441c36a5fbd28b8bc6481
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-07-06 18:06:36 +0400

    git commit -m "Modify files"

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,18 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    String color;
+    int numberOfPaws;
+    boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return "This animal is mostly " + color + ". " + "It has " + (numberOfPaws == 1 ? "1 paw": numberOfPaws + " paws") + " and " + (hasFur ? "a": "no") + " fur.";
+    }
+
 }
